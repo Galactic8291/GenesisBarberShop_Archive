d0d4b0ba2f2db94c140debe4089ce00623e17dfc
Robbie McClellan <robbiemcclellan14@gmail.com>
2017-06-23 12:36:53 -0400

    Reset the structure for rebuild

--- js/index.js
+++ js/index.js
@@ -1,13 +1,5 @@
 (function($) {
-  const htmlWindow = $(window),
-    mainContent = $('.main-content'),
-    menuButton = $('.menu-button'),
-    body = $('.body'),
-    header = $('.top-bar'),
-    trigger = $('.trigger'),
-    navigation = $('.navigation'),
-    button = $('.button'),
-    navigationLabel = $('.navigation-label')
+  const htmlWindow = $(window)
 
   const checkViewport = () => {
     return window.getComputedStyle(document.querySelector('.main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "")
@@ -15,29 +7,10 @@
 
   const moveItems = () => {
     const view = checkViewport()
-
-    if (view === 'mobile' || view === 'tablet') {
-      navigation.detach()
-      mainContent.append(navigation)
-
-      button.detach().insertAfter(navigation.find('ul'))
-      navigationLabel.detach().prependTo('.navigation ul:first-child')
-    } else if(view === 'desktop') {
-      button.detach()
-      navigationLabel.detach()
-      navigation.detach().insertAfter(header.find('.logo'))
-    }
   }
 
   htmlWindow.on('resize', event => {
     (!window.requestAnimationFrame) ? setTimeout(moveItems, 300) : window.requestAnimationFrame(moveItems)
   })
-
-  trigger.click(event => {
-    event.preventDefault()
-    $([menuButton, navigation, body]).toggleClass('nav-is-visible')
-  })
-
-  moveItems()
 })(jQuery)
 

c243eb76cec6183c797eb9439fb761619a68f905
Robbie McClellan <robbiemcclellan14@gmail.com>
2017-06-23 14:31:02 -0400

    Setup correct functionality of navigation and header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 (function($) {
   const htmlWindow = $(window),
-    navigationLabel = $('.navigation-label')
+    header = $('.top-bar'),
+    body = $('.body'),
+    mainContent = $('.main-content'),
+    navigation = $('.navigation'),
+    menuButton = $('.menu-button'),
+    trigger = $('.trigger'),
+    navigationLabel = $('.navigation-label'),
+    button = $('.button')
 
   const checkViewport = () => {
     return window.getComputedStyle(document.querySelector('.main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "")
@@ -10,14 +17,23 @@
     const view = checkViewport()
 
     if (view === 'mobile' || view === 'tablet') {
+      navigation.detach().prependTo(mainContent)
       navigationLabel.detach().prependTo('.navigation ul:first-child')
+      button.detach().appendTo('.navigation')
     } else if(view === 'desktop') {
+      navigation.detach().insertAfter(header.find('.logo'))
       navigationLabel.detach()
+      button.detach()
     }
   }
 
   htmlWindow.on('resize', event => {
     (!window.requestAnimationFrame) ? setTimeout(moveItems, 300) : window.requestAnimationFrame(moveItems)
+  })
+
+  trigger.click(event => {
+    event.preventDefault()
+    $([menuButton, navigation, body]).toggleClass('nav-is-visible')
   })
 
   moveItems()

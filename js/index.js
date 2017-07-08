8e72c926eb2f31e931d04f5685477787ff1831b1
Robbie McClellan <robbiemcclellan14@gmail.com>
2017-07-07 20:38:42 -0400

    Rebuild cuts to use pictures from instagram

--- js/index.js
+++ js/index.js
@@ -39,6 +39,17 @@
     $([menuButton, navigation, body]).toggleClass('nav-is-visible')
   })
 
+  let feed = new Instafeed({
+    get: 'user',
+    clientId: 'c71f283d688c4d5088c329c8575102b0',
+    userId: '179067665',
+    accessToken: '179067665.1677ed0.adf4d54f59854664932767899c6ddf60',
+    resolution: 'standard_resolution',
+    template: '<a href="{{link}}" class="cut"><img src="{{image}}" /></a>',
+    limit: 12
+  })
+
+  feed.run()
   moveItems()
 })(jQuery)
 
